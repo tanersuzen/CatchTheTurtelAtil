dd2cbc9df4c8de3cc5e1da26384649576e0adeab
Taner Süzen <taner.suzen95@gmail.com>
2023-11-25 22:48:16 +0300

    revision1

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ FONT = ("Arial",30,"normal")
 #score turtle
 
 score_turtle = turtle.Turtle()
-score_turtle.color("dark blue")
-score_turtle.write(arg="Score: 0", move=False,align="center",font=(FONT))
 
+def setup_score_turtle():
+    score_turtle.hideturtle()
+    score_turtle.color("dark blue")
+    score_turtle.penup()
 
+    top_height = screen.window_height() / 2
+    y = top_height * 0.9
+    score_turtle.setpos(0,y)
+    score_turtle.write(arg="Score: 0", move=False,align="center",font=(FONT))
 
+setup_score_turtle()
 
 
 turtle.mainloop()
