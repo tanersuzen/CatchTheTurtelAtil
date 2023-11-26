fc56e66fc7f4f456751019a1f78db4d3ca93b1af
Taner Süzen <taner.suzen95@gmail.com>
2023-11-26 11:40:42 +0300

    revision2

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import turtle
+import random
 
 screen = turtle.Screen()
 screen.bgcolor("light blue")
 screen.title("Catch the Turtle")
 FONT = ("Arial",30,"normal")
+scorenum = 0
+game_over = False
+
+#turtle list
+turtle_list = []
 
 #score turtle
-
 score_turtle = turtle.Turtle()
+
+#countdown turtle
+countdown_turtle = turtle.Turtle()
 
 def setup_score_turtle():
     score_turtle.hideturtle()
@@ -19,8 +27,71 @@ def setup_score_turtle():
     score_turtle.setpos(0,y)
     score_turtle.write(arg="Score: 0", move=False,align="center",font=(FONT))
 
-setup_score_turtle()
+grid_size = 10
+def make_turtle(x,y):
+    t = turtle.Turtle()
 
+    def handle_click(x,y):
+        global scorenum
+        scorenum += 1
+        score_turtle.clear()
+        score_turtle.write(arg=f"Score: {scorenum}", move=False, align="center", font=(FONT))
+
+    t.onclick(handle_click)
+    t.penup()
+    t.shape("turtle")
+    t.shapesize(2,2)
+    t.color("dark green")
+    t.goto(x * grid_size,y * grid_size)
+    turtle_list.append(t)
+
+
+
+def setup_turtles():
+    for y in range(-10,30,10):
+        for x in range(-20,30,10):
+            make_turtle(x,y)
+
+
+def hide_turtles():
+    for t in turtle_list:
+        t.hideturtle()
+
+#recursive func
+def show_turtles_randomly():
+    if not game_over:
+        hide_turtles()
+        random.choice(turtle_list).showturtle()
+        screen.ontimer(show_turtles_randomly,1000)
+
+def countdown(time):
+    global game_over
+    countdown_turtle.penup()
+    countdown_turtle.hideturtle()
+    top_height2 = screen.window_height() / 2
+    y2 = top_height2 * 0.8
+    countdown_turtle.setpos(0, y2)
+    countdown_turtle.clear()
+    if time > 0:
+        countdown_turtle.clear()
+        countdown_turtle.write(arg=f"Time: {time}",move=False,align="center",font=(FONT))
+        screen.ontimer(lambda: countdown(time-1),1000)
+    else:
+        game_over = True
+        countdown_turtle.clear()
+        hide_turtles()
+        countdown_turtle.write(arg="Game Over!",move=False,align="center",font=(FONT))
+
+def game_startup():
+    turtle.tracer(0)
+    setup_turtles()
+    hide_turtles()
+    setup_score_turtle()
+    show_turtles_randomly()
+    countdown(10)
+    turtle.tracer(1)
+
+game_startup()
 
 turtle.mainloop()
 
